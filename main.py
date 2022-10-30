2cb1d0b20dcb73d81524c2edc5ef800945e8a30b
JebSea <jeb.seawright@ufl.edu>
2022-10-30 14:48:17 -0400

    Added subtract

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def add(a, b):
     return a + b
+def subtract(a, b):
+    return a - b
 
 def main():
     if __name__ == '__main__':
         print(add(1, 4))
+        print(subtract(1, 4))
 main()
